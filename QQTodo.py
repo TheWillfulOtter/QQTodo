f01cc868134ec641f21cd4a09263f6e9d0aa864b
TheWillfulOtter <thewillfulotter@gmail.com>
2017-07-10 14:59:15 -0700

    Create QQTodo.py

---
+++ QQTodo.py
@@ -0,0 +1,38 @@
+"""
+The purpose of this code is to create a working todo list manager programe for my own use.
+Requrements:
+	Text based
+	Each task must have a project and a context
+	Projects are noted with a leading '+'
+	Contexts are notes with a leading '@'
+		i.e. Buy a new trashcan +home @store
+"""
+
+class Todo:
+	""" A todo item with the following properties
+		task: the description of the task itself as a string
+		project: the project associtated with this task as a string
+		context: the context in which the taks is most likely done as a string
+		state: the state of doneness of the task as a boolean T/F
+	"""
+	def __init__(self, task, project, context, state):
+		"""return a ToDo that is a *taks* with a *project* and a *context*
+		"""
+		self.task = task
+		self.project = project
+		self.context = context
+		self.state = True
+		
+
+newtask = input(str("What is the task?\n"))
+newproject = input(str("What is the project associated with this task? \n")) 
+newcontext = input(str("Where can this taks be done? \n"))
+state = True
+
+newTask = Todo(newtask, newproject, newcontext, True)
+
+"""
+newTask = Todo("Find out how to make this work", "+QQTodo", "@everywhere", True)
+"""
+
+print ("This is your new task:", newTask.task, newTask.project, newTask.context)
